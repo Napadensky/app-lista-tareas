3051691e993bf98ccd3780c2cdfa87da89bd8eba
David Napadensky <davidnapadensky@gmail.com>
2021-07-27 02:17:34 -0300

    agregando modelos de tareas

--- src/app.js
+++ src/app.js
@@ -1,15 +1,25 @@
 const {inquirerMenu, pause} = require('./helpers/inquirer');
+const {Tarea,Tareas} = require('./models');
 
 let opt = ''
 
 const main = async () => {
 
   do {
-    console.clear()
-    opt = await inquirerMenu()
+    // console.clear()
+    // opt = await inquirerMenu()
+
+    const tarea = new Tarea('Comprar comida')
+    console.log(tarea);
+    
+    const tareas = new Tareas()
+    tareas._listado[tarea.id] = tarea
+    console.log(tareas);
+
+
     await pause()
   } while (opt !== '0')
-  
+
 }
 
 main()
---
+++ src/models/index.js
@@ -0,0 +1,7 @@
+const Tarea = require('./Tarea')
+const Tareas = require('./Tareas')
+
+module.exports = {
+  Tarea,
+  Tareas,
+}
